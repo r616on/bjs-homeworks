374f5dd0aa2c51548f0901d95d822d9eba8131e9
Roman Prilepskiy <r616on@gmail.com>
2021-01-02 16:14:43 +0300

    home_works_2_2_Done

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,50 @@
-//String.prototype.isPalindrome - для задачи №1
+String.prototype.isPalindrome = function () {
+    let palindrome, reversPalindrome = "", newPalindrome = "";
+    palindrome = this.toUpperCase();
+
+    for (let i = 0; i < palindrome.length; i++) {
+        if (palindrome[i] === " ") {
+        } else {
+            newPalindrome = newPalindrome + palindrome[i];
+        };
+
+    };
+    for (let i = (newPalindrome.length - 1); i >= 0; i--) {
+        reversPalindrome = reversPalindrome + newPalindrome[i];
+    };
+    if (newPalindrome === reversPalindrome) {
+        return true;
+    } else {
+        return false;
+    };
+
+}
+
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    if (marks.length === 0) {
+        return 0;
+    }
+    let average = 0, roundedAverage = 0;
+    for (let i = 0; i < marks.length; i++) {
+        average = average + (+marks[i]);
+    };
+    average = average / marks.length;
+    roundedAverage = Math.round(average);
+    return roundedAverage;
+
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    const = new Date().valueOf();
+    birthday = new Date(birthday).valueOf();
+    const diff = now - birthday;
+    const age = diff / 1000 / 60 / 60 / 24 / 365.25;
+    if (age > 18) {
+        return true
+    } else {
+        return false
+    };
+
+
 }
